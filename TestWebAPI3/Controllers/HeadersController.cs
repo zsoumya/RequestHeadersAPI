2a4b903a8c7fdaf97bc533585f5bfde8f10ece13
Soumya Mukherjee <zsoumya@yahoo.com>
2018-11-25 14:35:28 -0500

    Add project files.

---
+++ TestWebAPI3/Controllers/HeadersController.cs
@@ -0,0 +1,40 @@
+﻿namespace TestWebAPI3.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Primitives;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeadersController : ControllerBase
+    {
+        [HttpGet("{key?}")]
+        public ActionResult<IDictionary<string, string>> Get(string key)
+        {
+            IHeaderDictionary requestHeaders = this.HttpContext.Request.Headers;
+            Dictionary<string, string> dictionary;
+
+            if (!string.IsNullOrWhiteSpace(key))
+            {
+                dictionary = new Dictionary<string, string>
+                {
+                    {
+                        key,
+                        !StringValues.IsNullOrEmpty(requestHeaders[key]) ? string.Join('|', requestHeaders[key]) : "--NULL--"
+                    }
+                };
+            }
+            else
+            {
+                dictionary = requestHeaders.ToDictionary(requestHeader => requestHeader.Key,
+                                                         requestHeader => string.Join('|', requestHeader.Value.ToArray()));
+            }
+
+            return dictionary;
+        }
+    }
+}
